7279540bd7b938633d861cc7b640d77c2550756f
Zarak Khan <zedd0099@gmail.com>
2024-05-12 17:14:07 +0500

    fixing bug

--- apps/web/app/api/auth/logout/route.js
+++ apps/web/app/api/auth/logout/route.js
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
   const cookie = serialize("token", "", {
     httpOnly: true,
+    // eslint-disable-next-line turbo/no-undeclared-env-vars
     secure: process.env.NODE_ENV !== "development",
     expires: new Date(0),
     sameSite: "lax",
